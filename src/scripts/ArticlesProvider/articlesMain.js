3a1ddb8281b14f1047d548b6d10407a18c018c3e
jonrrile <jonrrile@ut.utm.edu>
2020-06-26 14:45:59 -0500

    user can now add a new article object, however I havent figured out how to succesfully clearInput which is causing the entries to repopulate.

--- src/scripts/ArticlesProvider/articlesMain.js
+++ src/scripts/ArticlesProvider/articlesMain.js
@@ -6,6 +6,13 @@ import createArticleEntry from "./articlesFactory.js"
 //fetch all of the articles then render them
 API.getAllArticles().then((response) => renderArticleEntries(response));
 
+//clear input function for the article objects
+const clearInputs = () => {
+    document.querySelector(".news__title").value = "";
+    document.querySelector(".news__synopsis").value = "";
+    document.querySelector(".news__url").value = "";
+}
+
 //code in progress to allow user to utilize factory function to create new entry
 //alert will make sure they have filled out all required fields. 
 const recordArticleEntry = document.querySelector(".save__article")
@@ -17,5 +24,13 @@ recordArticleEntry.addEventListener("click", event => {
     if (title === "" || synopsis === "" || url === "") {
         alert("Please fill out all fields!")
 
+    } else {
+        //if all fields are filled out will create a new article object
+        let newArticle = createArticleEntry(title, synopsis, url)
+        console.log(newArticle)
+        API.saveArticleEntry(newArticle).then(() => {
+            clearInputs();
+            API.getAllArticles().then((response) => renderArticleEntries(response));
+        })
     }
 })
--- src/scripts/ArticlesProvider/articlesProvider.js
+++ src/scripts/ArticlesProvider/articlesProvider.js
@@ -3,6 +3,16 @@ const API = {
     getAllArticles: () => {
         return fetch("http://localhost:3000/articles")
         .then(response => response.json());
+    },
+
+    saveArticleEntry: (newEntryObject) => {
+        return fetch("http://localhost:3000/articles", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newEntryObject)
+        })
     }
 }
 
