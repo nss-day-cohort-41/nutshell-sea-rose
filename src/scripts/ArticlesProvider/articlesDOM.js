1a01f1cb8bbbbbcfab10c22a969ad2f6410d57da
jonrrile <jonrrile@ut.utm.edu>
2020-06-26 15:58:14 -0500

    final changes for today

--- src/scripts/ArticlesProvider/articlesDOM.js
+++ src/scripts/ArticlesProvider/articlesDOM.js
@@ -5,7 +5,8 @@ const makeArticleHTML = ( articleObj ) => {
 	<h3>Article:</h3>
 	<p>${articleObj.title}</p>
 	<p>${articleObj.synopsis}</p>
-	<p>${articleObj.url}</p>
+    <p>${articleObj.url}</p>
+    <button id="delete__article">Delete</button>
 	</div>
 	`
 	return domElement
--- src/scripts/ArticlesProvider/articlesMain.js
+++ src/scripts/ArticlesProvider/articlesMain.js
@@ -7,14 +7,19 @@ import createArticleEntry from "./articlesFactory.js"
 API.getAllArticles().then((response) => renderArticleEntries(response));
 
 //clear input function for the article objects
+
+/* working on this part still.
 const clearInputs = () => {
     document.querySelector(".news__title").value = "";
     document.querySelector(".news__synopsis").value = "";
-    document.querySelector(".news__url").value = "";
+    document.querySelector(".news__url").value = ""
 }
+/*
+*/ 
 
 //code in progress to allow user to utilize factory function to create new entry
 //alert will make sure they have filled out all required fields. 
+
 const recordArticleEntry = document.querySelector(".save__article")
 recordArticleEntry.addEventListener("click", event => {
     const title = document.querySelector(".news__title").value
@@ -28,9 +33,16 @@ recordArticleEntry.addEventListener("click", event => {
         //if all fields are filled out will create a new article object
         let newArticle = createArticleEntry(title, synopsis, url)
         console.log(newArticle)
-        API.saveArticleEntry(newArticle).then(() => {
-            clearInputs();
+        API.saveArticleEntry(newArticle)//.then(() => {
             API.getAllArticles().then((response) => renderArticleEntries(response));
-        })
+        //})
     }
 })
+
+const deleteArticleEntry = document.querySelector("#delete__article")
+deleteArticleEntry.addEventListener("click", event => {
+    if (event.target.id.startsWith("delete__article")) {
+        alert("Delete!")
+    }
+})
+
