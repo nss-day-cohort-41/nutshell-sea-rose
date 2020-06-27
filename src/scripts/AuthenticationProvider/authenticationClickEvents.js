258b9c71955604ab0ea8f4faf3f456c3f01a991f
Zach McWhirter <zmac7811@gmail.com>
2020-06-26 20:24:43 -0500

    added new way to test authentication on authProv.js

--- src/scripts/AuthenticationProvider/authenticationClickEvents.js
+++ src/scripts/AuthenticationProvider/authenticationClickEvents.js
@@ -1,21 +1,35 @@
 
-import API from "./authenticationProvider.js"
+import authenticationAPI from "./authenticationProvider.js"
 
-const loginSubmitButton = document.querySelector("#login__button")
+const authenticationAPI = {
+	createUser(username, password) {
+		alert(`username: ${username}, password: ${password}`);
+	}
+}
 
-   const registerLoginListener = () => {
-        loginSubmitButton.addEventListener("click", event => {
-            console.log("I click first", event.target.id)
-            const userLogin = document.querySelector("#loginUsername").value
-            const passwordLogin = document.querySelector("#loginPassword").value
-            API.getSingleUser(userLogin)
-            .then((loginArray) => {
-                // clearInputs()
-                console.log(userLogin)
-                console.log(loginArray)
-                console.log("click last")
-            })
-        })  
-   }
 
-export default registerLoginListener
+
+
+
+
+
+
+
+// const loginSubmitButton = document.querySelector("#login__button")
+
+//    const registerLoginListener = () => {
+//         loginSubmitButton.addEventListener("click", event => {
+//             console.log("I click first", event.target.id)
+//             const userLogin = document.querySelector("#loginUsername").value
+//             const passwordLogin = document.querySelector("#loginPassword").value
+//             auththenticationAPI.getSingleUser(userLogin)
+//             .then((loginArray) => {
+//                 // clearInputs()
+//                 console.log(userLogin)
+//                 console.log(loginArray)
+//                 console.log("click last")
+//             })
+//         })  
+//    }
+
+// export default registerLoginListener
--- src/scripts/AuthenticationProvider/authenticationProvider.js
+++ src/scripts/AuthenticationProvider/authenticationProvider.js
@@ -1,20 +1,30 @@
 //Setting url will be used for all fetch calls
 const url = "http://localhost:3000";
 
-const API = {
-    getSingleUser: (id) => {
-        return fetch(`${url}/users/${id}`)
-        .then(response => response.json());
-    },
 
-    addAUser: (userObj) => {
-		return fetch(`${url}/users`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify(userObj)
-		}).then(response => response.json());
-    }   
+
+const authenticationAPI = {
+	createUser(username, password) {
+		alert(`username: ${username}, password: ${password}`);
+	}
 }
-export default API;
+
+
+
+// const auththenticationAPI = {
+//     getSingleUser: (id) => {
+//         return fetch(`${url}/users/${id}`)
+//         .then(response => response.json());
+//     },
+
+//     addAUser: (userObj) => {
+// 		return fetch(`${url}/users`, {
+// 			method: "POST",
+// 			headers: {
+// 				"Content-Type": "application/json"
+// 			},
+// 			body: JSON.stringify(userObj)
+// 		}).then(response => response.json());
+//     }   
+// }
+export default auththenticationAPI;
