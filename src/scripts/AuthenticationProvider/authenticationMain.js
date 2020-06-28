aee690b9a86db64aa93bb3f1a3ce939c337cda87
Zach McWhirter <zmac7811@gmail.com>
2020-06-28 18:21:11 -0500

    cleared out unued modules and started the process of creating new users

--- src/scripts/AuthenticationProvider/authenticationMain.js
+++ src/scripts/AuthenticationProvider/authenticationMain.js
@@ -1,2 +1,76 @@
-import blankObject from "./authenticationClickEvents.js";
+import authenticationAPI from "./authenticationProvider.js"
 
+
+const authenticationSubmitButton = document.querySelector('#authentication-submit-button');
+const createAccountSubmitButton = document.querySelector('#create-account-submit-button');
+const signOutButton = document.querySelector("#signout-button")
+
+const userExists = (userList, username) => {
+    for (let user of userList) {
+        if (user.username === username) {
+            return true
+        }
+    }return false
+}
+
+// check to see if anything is in storage
+
+const setupAuthenticationButtons = () => {
+    let userId = sessionStorage.getItem("activeUser");
+
+    if(!userId) {
+        // user IS NOT signed in, so only sign in and create account buttons are visible.
+        authenticationSubmitButton.disabled = false;
+		createAccountSubmitButton.disabled = false;
+		signOutButton.disabled = true;
+    } else {
+        // user IS signed in, so only sign out button is visible.
+        authenticationSubmitButton.disabled = true;
+		createAccountSubmitButton.disabled = true;
+		signOutButton.disabled = false;
+    }
+}
+
+setupAuthenticationButtons()
+
+// Events section
+
+
+//user sign IN event
+authenticationSubmitButton.addEventListener('click', e => {
+    const usernameInput = document.querySelector("#username");
+    const passwordInput = document.querySelector("#password")
+    authenticationAPI.signInUser(usernameInput.value, passwordInput.value);
+});
+
+//user sign OUT event
+signOutButton.addEventListener('click', e => {
+	sessionStorage.removeItem('activeUser');
+	setupAuthenticationButtons();
+});
+
+//create new account event
+createAccountSubmitButton.addEventListener('click', e => {
+	const username = document.querySelector("#createUsername").value;
+    const password = document.querySelector("#createPassword").value;
+    const email = document.querySelector("#email").value;
+
+    // get all the users
+    authenticationAPI.getAllUsers()
+        .then(allUsers => {
+                // iterate through and see if a user already exists
+
+
+
+
+            //then create the user
+            authenticationAPI.createNewUser(username, email, password)
+                .then(createdUser => {
+                    // save user to localstorage
+                    sessionStorage.setItem("activeUser", createdUser.id);
+                })
+        })
+
+        //if the user exists, display an error message of some kind, and then exit, clear and back to the form
+        authenticationAPI.createdUser(newUsernameInput, emailInput, newUserPasswordInput)
+    });
--- src/scripts/AuthenticationProvider/authenticationProvider.js
+++ src/scripts/AuthenticationProvider/authenticationProvider.js
@@ -4,22 +4,67 @@ const url = "http://localhost:3000/users"
 
 // These will be altered to become our fetch calls when we are ready for them
 
+// let nextID = 3;
+// const allUsers = [
+// 	{
+// 		id: 1,
+// 		username: 'bob',
+// 		email: 'bob@bob.com',
+// 		password: 'bobspassword'
+// 	},
+// 	{
+// 		id: 2,
+// 		username: 'Sue',
+// 		email: 'sue@sue.com',
+// 		password: 'suespassword'
+// 	}
+// ];
+
 
 const authenticationAPI = {
+
+	getAllUsers() {
+		return new Promise(function (resolve, reject) {
+			return resolve(allUsers);
+		});
+
+
+		// return fetch(`${url}/users`)
+		// .then(response => response.json());
+	},
+
+
 	signInUser() {
 		console.log((`${url}.value`))
 		
 	},
 	createNewUser(username, email, password) {
-		return fetch(`username: ${username}, password: ${password},email: ${email}`)
-		.then(response => response.json());
+		return new Promise(function (resolve, reject) {
+			let newUser = { 
+				id: nextID++,
+				username: username, 
+				password: password,
+			 	email: email
+			};
+			allUsers.push(newUser);
+			return resolve(newUser);
+		});
+	
+
+
 	} 
 }
 
+		// return fetch(`username: ${username}, password: ${password},email: ${email}`)
+		// .then(response => response.json());
 
 
-
-
+// return fetch(`${url}/users`, {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json' },
+		// 	body: JSON.stringify({ username: username, email: email, password: password })
+		// }).then(response => response.json());
 
 // url.forEach(user => {
 
