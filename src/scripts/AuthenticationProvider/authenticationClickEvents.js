c9b64ea6bdaefbe4ef25b5499fdd39f2112d6cfc
nickglovdev <nickglovdev@gmail.com>
2020-06-28 16:37:11 -0500

    Added a quick comment about the author

--- src/scripts/AuthenticationProvider/authenticationClickEvents.js
+++ src/scripts/AuthenticationProvider/authenticationClickEvents.js
@@ -1,18 +1,24 @@
 import API from "./authenticationProvider.js"
 
+// Written by Nick Glover
 const userLogin = document.querySelector("#loginUsername")
 const passwordLogin = document.querySelector("#loginPassword")
 const submitLoginButton = document.querySelector("#loginButton")
 
+// Login event for the webpage
 const sessionStorageLogIn = () => {
     submitLoginButton.addEventListener('click', event => {
         API.getAllUsers()
             .then(userArray => {
                 let user = userArray.find((user) => {
+
+                    // Validation for password and username
                     if (passwordLogin.value === user.password && user.user === userLogin.value) {
                         return user.user === userLogin.value
                     } 
                 })
+
+                // We decided to use a key value pairs for our session storage.
                 sessionStorage.id = user.id
                 sessionStorage.user = user.user
                 console.log(sessionStorage)
