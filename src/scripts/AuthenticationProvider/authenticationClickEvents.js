d6fcaa1996d653f10bc47784f211be392298dbca
Zach McWhirter <zmac7811@gmail.com>
2020-06-28 21:22:44 -0500

    saving because the power went out

--- src/scripts/AuthenticationProvider/authenticationClickEvents.js
+++ src/scripts/AuthenticationProvider/authenticationClickEvents.js
@@ -4,6 +4,8 @@ import API from "./authenticationProvider.js"
 const userLogin = document.querySelector("#loginUsername")
 const passwordLogin = document.querySelector("#loginPassword")
 const submitLoginButton = document.querySelector("#loginButton")
+const createAccountButton = document.querySelector("#create-account-button")
+
 
 // Login event for the webpage
 const sessionStorageLogIn = () => {
@@ -11,18 +13,37 @@ const sessionStorageLogIn = () => {
         API.getAllUsers()
             .then(userArray => {
                 let user = userArray.find((user) => {
-
+                    
                     // Validation for password and username
                     if (passwordLogin.value === user.password && user.user === userLogin.value) {
                         return user.user === userLogin.value
+
                     } 
+                    // else if(user) {
+                    //     // user IS NOT signed in, so only sign in and create account buttons are visible.
+                    //     authenticationSubmitButton.disabled = false;
+                    //     createAccountSubmitButton.disabled = false;
+                    //     signOutButton.disabled = true;
+                    // } else {
+                    //     // user IS signed in, so only sign out button is visible.
+                    //     authenticationSubmitButton.disabled = true;
+                    //     createAccountSubmitButton.disabled = true;
+                    //     signOutButton.disabled = false;
+                    // }
                 })
 
                 // We decided to use a key value pairs for our session storage.
                 sessionStorage.id = user.id
                 sessionStorage.user = user.user
                 console.log(sessionStorage)
+                
             })
+    })
+}
+
+const createNewUserAccount = () => {
+    createAccountButton.addEventListener("click", event => {
+        API.createUser()
     })
 }
 
--- src/scripts/AuthenticationProvider/authenticationProvider.js
+++ src/scripts/AuthenticationProvider/authenticationProvider.js
@@ -2,15 +2,34 @@
 const API = {
 
     getAllUsers: () => {
-      return fetch(`http://localhost:3000/users/`)
+      return fetch("http://localhost:3000/users/")
       .then(response => response.json());
     },
   
-    getSingleUser: (id) => {
-    //return fetch(`${url}/users/${id}`)
-    return fetch(`http://localhost:3000/Users/${id}`)
-		.then(response => response.json());
-    },
+    // getSingleUser: (id) => {
+    // //return fetch(`${url}/users/${id}`)
+    // return fetch(`http://localhost:3000/Users/${id}`)
+	// 	.then(response => response.json());
+	// },
+	createUser: (username, email, password) => {
+		return fetch(`${url}/users`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(username, email, password)
+		}).then(response => response.json())
+		
+
+	}
+	
+	// createUser: (username, email, password) => {
+	// 	return fetch(`${url}/users`,
+	// 		method: 'POST',
+	// 		headers: { 'Content-Type': 'application/json' },
+	// 		body: JSON.stringify({ username: username, email: email, password: password })
+		
+	// })		.then(response => response.json());
 }
 
 export default API;
