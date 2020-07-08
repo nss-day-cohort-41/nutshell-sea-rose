96bb7da3c680eed5c7516da08119a9e17bfa882c
Zach McWhirter <zmac7811@gmail.com>
2020-07-07 23:08:08 -0500

    flexbox styling

--- src/scripts/FriendsProvider/friendsDOM.js
+++ src/scripts/FriendsProvider/friendsDOM.js
@@ -5,11 +5,11 @@ const makeFriendHTML = (friendObj) => {
     <div class="user__card">
     <p class="hideId">${friendObj.id}</p>
     <h2>${friendObj.username}</h2>
-    <img src="https://visualpharm.com/assets/448/User-595b40b85ba036ed117dbd22.svg" alt="">
-	<button id="deleteUser--${friendObj.id}">Delete Friend</button>
-	
-	`
+	<button id="deleteUser--${friendObj.id}">Delete Friend</button>`
     return domElement
 }
 
 export default makeFriendHTML;
+
+// Here is the friend icon if you want to use it
+// {<img src="https://visualpharm.com/assets/448/User-595b40b85ba036ed117dbd22.svg" alt=""></img>}
